e2ee4d1fe9da65475e113cad76b75083fd00a8ec
Cuongyd196 <cuongyd196@gmail.com>
2026-07-19 20:05:58 +0700

    #update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,10 +11,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://Cuongyd196.github.io',
+  url: 'https://devops.cuongit.net',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/devops-resources/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
