75707d6397b2cd1e2c4a7cec3f07cd8cb8775610
Cuongyd196 <cuongyd196@gmail.com>
2026-07-19 20:02:14 +0700

    #update config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,15 +11,16 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://devops.cuongit.net',
+  url: 'https://Cuongyd196.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/devops-resources/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
+  organizationName: 'Cuongyd196', // Usually your GitHub org/user name.
   projectName: 'devops-resources', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
